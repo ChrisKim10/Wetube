d00ffff27e05551586ba225b266188b61dd13957
ChrisKim <ChrisKim10@live.co.kr>
2019-12-05 17:16:32 +0900

    [ADD] ViewModel, Prism

---
+++ YoutubeExplode.DemoUWP/ViewModel/YoutubeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism;
+using Prism.Mvvm;
+
+namespace YoutubeExplode.DemoUWP.ViewModel
+{
+    public class YoutubeViewModel: BindableBase
+    {
+    }
+}
